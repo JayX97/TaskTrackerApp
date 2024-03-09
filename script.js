085986d7b802a8bf077acbe87743e1b70980ce8b
JayX97 <jason.fernandezx@yahoo.com>
2024-03-08 19:59:41 -0500

    Implement task rendering

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 // TODO: Call the render function to update the table with the new tasks.
 
 //Section 2: App State Variables
-let task = [];
+let tasks = [];
 
 //Section 3 Cached Element References
 const taskForm = document.getElementById("taskForm");
@@ -13,3 +13,44 @@ const taskTable = document.getElementById("taskTable");
 
 console.log(taskForm);
 console.log(taskTable);
+
+//function to handle submissions
+const handleSubmission = event => {
+    event.preventDefault();
+
+    // TODO: Get form input values
+    const taskName = document.getElementById("taskName").value;
+    const taskDescription = document.getElementById("taskDescription").value;
+    const taskDeadline = document.getElementById("taskDeadline").value;
+    
+    // TODO: Validate input fields
+    if (taskName === "" && taskDeadline === "") {
+        alert("Task name and deadline are required!");
+        return;
+    }
+    
+    // TODO: Update the tasks array
+    tasks.push({ name: taskName, description: taskDescription, deadline: taskDeadline});
+    
+    render();
+}
+
+//function to render tasks in the table
+const render = () => {
+    // TODO: Use array methods to create a new table row of data for each item in the arr
+    taskTable.innerHTML = tasks.map(task => `
+    <tr>
+        <td>${task.name}</td>
+        <td>${task.description}</td>
+        <td>${task.deadline}</td>
+        <td><button onclick="markTaskComplete(this)">Complete</button></td>
+        <td><button onclick="removeTask(this)">Remove</button></td>
+    `).join('');
+}
+
+//function to initialize the table
+const init = () => {
+    taskTable.innerHTML = ''; //clear the table
+    tasks = []; //resets the tasks array
+    render(); //call the render function
+}
